d96bbe4c3759bdd13f2a681e8637629b864d6b65
priyeshjain35 <priyeshjain35@gmail.com>
2019-05-09 19:32:52 +0530

    updated folder name as github folder name.

--- src/view/Routes.js
+++ src/view/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
-import About from './components/About/About';
+import About from './components/about/About';
 import Education from '../view/components/Education/Education';
 import Projects from '../view/components/Projects/Projects';
 import Contact from '../view/components/Contact/Contact';
